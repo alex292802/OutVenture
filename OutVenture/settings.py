c9bad0cad7372e33549eb4b3c9e30a73a8bf5ee0
alex292802 <alexandre.berger2802@gmail.com>
2024-12-14 14:06:00 +0100

    feature: handling CORS

--- OutVenture/settings.py
+++ OutVenture/settings.py
@@ -15,6 +15,15 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:3000',
+]
+
+CORS_ALLOW_CREDENTIALS = True  # Allow credentials (cookies, tokens)
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',  # Frontend origin
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -37,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'OutVenture.urls'
