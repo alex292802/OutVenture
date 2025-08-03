579677d2945d349e42781960b3739021cc30f45d
alex292802 <alexandre.berger2802@gmail.com>
2025-08-03 19:34:17 +0200

    refactor: translating login.js

--- outventure-frontend/src/components/Login.js
+++ outventure-frontend/src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { Form, Input, Button, Checkbox, Card, message } from 'antd';
+import { Form, Input, Button, Checkbox, Card, message, Space } from 'antd';
 import { AuthContext } from '../context/AuthContext';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
@@ -11,17 +11,17 @@ const Login = ({ onLoginSuccess }) => {
     setLoading(true);
     try {
       await login(values);
-      message.success('Login successful!');
+      message.success('Connexion réussie');
       onLoginSuccess();
     } catch (error) {
-      message.error('Login failed. Please check your credentials.'); 
+      message.error('Échec de la connexion, veuillez vérifier vos identifiants'); 
     } finally {
       setLoading(false); 
     }
   };
 
   return (
-    <Card title="Login" style={{ width: 300, margin: '20px auto' }}>
+    <Card title="Connexion" style={{ width: 300, margin: '20px auto' }}>
       <Form
         name="normal_login"
         className="login-form"
@@ -30,36 +30,38 @@ const Login = ({ onLoginSuccess }) => {
       >
         <Form.Item
           name="username"
-          rules={[{ required: true, message: 'Please input your Username!' }]}
+          rules={[{ required: true, message: "Veuillez saisir votre nom d'utilisateur" }]}
         >
           <Input 
             prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Username" 
+            placeholder="Nom d'utilisateur" 
           />
         </Form.Item>
         <Form.Item
           name="password"
-          rules={[{ required: true, message: 'Please input your Password!' }]}
+          rules={[{ required: true, message: "Veuillez saisir votre mot de passe" }]}
         >
           <Input
             prefix={<LockOutlined className="site-form-item-icon" />}
             type="password"
-            placeholder="Password"
+            placeholder="Mot de passe"
           />
         </Form.Item>
         <Form.Item>
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
+          <Space direction="vertical">
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Se souvenir de moi</Checkbox>
+            </Form.Item>
 
-          <a className="login-form-forgot" href="">
-            Forgot password
-          </a>
+            <a className="login-form-forgot" href="">
+              Mot de passe oublié
+            </a>
+          </Space>
         </Form.Item>
 
         <Form.Item>
           <Button type="primary" htmlType="submit" className="login-form-button" style={{ width: '100%' }} loading={loading}>
-            Log in
+            Se connecter
           </Button>
         </Form.Item>
       </Form>
