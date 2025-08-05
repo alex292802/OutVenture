c71e94ab2018753a29682f610f9876b1e0d8f55e
alex292802 <alexandre.berger2802@gmail.com>
2025-08-05 18:11:38 +0200

    feature: enhancing AuthContext

--- outventure-frontend/src/context/AuthContext.js
+++ outventure-frontend/src/context/AuthContext.js
@@ -7,20 +7,72 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
+  const [refreshToken, setRefreshToken] = useState(null);
   const [loading, setLoading] = useState(false);
-  const baseRoute = "/api";
 
   const login = async (credentials) => {
     setLoading(true);
-    const response = await axios.post(`${baseRoute}/token/`, credentials);
-    setUser(response.data); // Store user data or token as needed
-    setLoading(false);
-    return response.data; // Return the response for further handling if needed
+    try {
+      const response = await axios.post(`/token/`, credentials);
+      const { access, refresh } = response.data;
+      saveTokens(access, refresh);
+      // TODO: setUser with only User informations. Make an other request to request informations
+      setUser(response.data);
+      return response.data;
+    } catch (error) {
+      throw error;
+    } finally {
+      setLoading(false);
+    }
   };
 
+  const saveTokens = (access, refresh) => {
+    setAccessToken(access);
+    setRefreshToken(refresh);
+    // this allows to persist user informations even after a refresh
+    localStorage.setItem('accessToken', access);
+    localStorage.setItem('refreshToken', refresh);
+  };
+
+  const loadTokens = () => {
+    const storedAccessToken = localStorage.getItem('accessToken');
+    const storedRefreshToken = localStorage.getItem('refreshToken');
+    if (storedAccessToken && storedRefreshToken) {
+      setAccessToken(storedAccessToken);
+      setRefreshToken(storedRefreshToken);
+    }
+  };
+
+  // TODO: should this triggers something in backend ?
   const logout = () => {
     setUser(null);
+    setAccessToken(null);
+    setRefreshToken(null);
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
   };
+
+  // TODO: Load user data on page refresh (if tokens exist). Currently, if the page reloads and tokens exist, the user stays null.
+  useEffect(() => {
+    loadTokens();
+  }, []);
+
+  // This allows to pass the token in every outgoing request
+  // TODO: handle 401 (expired token) here, we should use the refresh token to obtaine a new access token
+  useEffect(() => {
+    const requestInterceptor = axios.interceptors.request.use(
+      (config) => {
+        if (accessToken) {
+          config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
+
+    return () => axios.interceptors.request.eject(requestInterceptor);
+  }, [accessToken]);
 
   return (
     <AuthContext.Provider value={{ user, loading, login, logout }}>
