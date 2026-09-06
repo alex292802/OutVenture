ac2d0e58a6c357ec03794592d2f9c94c08bcbaa7
Alexandre Berger <alexandre.berger@citalid.com>
2026-09-06 17:46:39 +0200

    feature: separing PublicUserSerializer from User

--- core/serializers.py
+++ core/serializers.py
@@ -8,29 +8,39 @@ class ActivitySerializer(serializers.ModelSerializer):
         model = Activity
         fields = ["id", "name"]
 
-class UserSerializer(serializers.ModelSerializer):
+
+class PublicUserSerializer(serializers.ModelSerializer):
+    """Minimal, publicly shareable representation of a user.
+
+    Exposes only non-sensitive identity fields, so it can be embedded in
+    other payloads (e.g. review authors) without leaking location data.
+    """
+
     class Meta:
         model = User
-        fields = [
-            "id",
-            "public_name",
-        ]
+        fields = ["id", "public_name"]
+
 
 class ReviewSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+    user = PublicUserSerializer(read_only=True)
+
     class Meta:
         model = Review
         fields = ["date", "rating", "comment", "user"]
 
+
 # TODO: add a global score, based on reviews (average)
 class SpotSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
+
     class Meta:
         model = Spot
         fields = ["id", "name", "longitude", "latitude", "activities", "reviews"]
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """Full representation of a user, including their own geolocation data."""
+
     class Meta:
         model = User
         fields = [
@@ -38,5 +48,5 @@ class UserSerializer(serializers.ModelSerializer):
             "public_name",
             "longitude",
             "latitude",
-            "activities"
+            "activities",
         ]
